60b5ab9f3a978062d6898edde817e6ef6c57f468
nikhilbhatt10798 <83267123+nikhilbhatt10798@users.noreply.github.com>
2023-02-10 19:32:41 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const fieldsData = {
+  fromDate: 'Thu Feb 09 2023 00:00:00 GMT+0530 (India Standard Time)',
+  toDate: 'Sat Feb 18 2023 00:00:00 GMT+0530 (India Standard Time)',
+  selectedWeekDays:[
+    {
+        "label": "Mon",
+        "value": true
+    },
+    {
+        "label": "Tue",
+        "value": true
+    },
+    {
+        "label": "Wed",
+        "value": false
+    },
+    {
+        "label": "thu",
+        "value": true
+    },
+    {
+        "label": "Fri",
+        "value": false
+    },
+    {
+        "label": "Sat",
+        "value": false
+    },
+    {
+        "label": "Sun",
+        "value": false
+    }
+  ]
+};
+
+console.log(fieldsData.fromDate,fieldsData.toDate)
